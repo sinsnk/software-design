b0e2810d8dd39662e964a1f3901178635818db5f
umbrellajin <cb.so.devope@gmail.com>
2015-06-10 23:40:07 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -9,7 +9,13 @@ ERROR_EXIT () {
 }
 chmod 711 ./GreatestCommonDivisor.bash
 ANS=$(bash ./GreatestCommonDivisor.bash 24 32)
-if [ "$ANS" -eq 0 ] ; then
+if [ "$ANS" -eq 8 ] ; then
+  exit 0
+else
+  exit 1
+fi
+ANS=$(bash ./GreatestCommonDivisor.bash 125 65)
+if [ "$ANS" -eq 5 ] ; then
   exit 0
 else
   exit 1
