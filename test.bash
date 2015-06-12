9df195dcce58d7c7ea305598969db74eba68e59e
umbrellajin <cb.so.devope@gmail.com>
2015-06-12 22:30:15 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -7,6 +7,8 @@ ERROR_EXIT () {
   rm -f $tmp-*
   exit 1
 }
+
+# 正
 ANS=$(bash ./GreatestCommonDivisor.bash 24 32)
 if [ "$ANS" -eq 8 ] ; then
   exit 0
@@ -20,5 +22,12 @@ else
   exit 1
 fi
 
+# 誤
+ANS=$(bash ./GreatestCommonDivisor.bash 125 65)
+if [ "$ANS" -eq 1 ] ; then
+  exit 1
+else
+  exit 0
+fi
 # 変な値
 # bash ./GreatestCommonDivisor.bash hoge fuga
